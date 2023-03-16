c281b54502af49bd8d65d5713239f8758735b3d2
AllanOps <ombongiallan@gmail.com>
2023-03-16 19:57:06 +0300

    Printing a specified string using puts function of the standard library

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Main: Function entry point
+ *
+ * Puts: function from stdio pronting a string
+ *
+ * Return value: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like a multilingual puzzle");
+
+	return (0);
+}
