cb41306cc1b1421a695792a94664fa07f0997633
AllanOps <ombongiallan@gmail.com>
2023-03-21 02:21:30 +0300

    Print combinations of two numbers - Hot fixes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,30 +6,34 @@
  */
 int main(void)
 {
-        int i, e;
-        
-        i = 48;
-        e = 48;
+	int i, e;
 
-        while (e < 58)
-        {
-                i = 48;
-                while (i < 58)
-                {
-                        if (e != i && e < i)
-                        {
-                                putchar(e);
-                                putchar(i);
-                                if (i == 57 && e == 56)
-                                {
-                                        break;
-                                }
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        i++;
-                }e++;
-        }
-        putchar('\n');
-        return (0);
+	i = 48;
+
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+
+	}
+
+	putchar('\n');
+	return (0);
 }
