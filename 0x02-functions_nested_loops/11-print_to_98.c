5d298de953ffcb5f33947aed0851a1013ad94101
AllanOps <ombongiallan@gmail.com>
2023-03-23 03:30:47 -0700

    Print numbers regardless of sign up to 98 - hot header file fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_to_98 - Print numbers to 98
  * @n: Number to be passed to function
- * @x: Number to be used as difference to 98
  * Return: Nothing
  */
 void print_to_98(int n)
