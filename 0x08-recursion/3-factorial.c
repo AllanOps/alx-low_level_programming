33b7b8b91a6c9ec057741e3ef4257ad38560381f
AllanOps <ombongiallan@gmail.com>
2023-07-05 14:32:02 +0300

    Factorial of a number via recursion - Hot doccumentation fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * factorial - Get the factorial of a specified number.
+ * factorial - Get the factorial of n
  * @n: Number to be factorized
- * return: 1, upon success
- * Returns -1 on error
+ *
+ * Return: 1, on success
+ * -1, on error, and errno set appropriately
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
 }
