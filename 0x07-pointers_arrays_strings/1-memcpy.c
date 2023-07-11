73c79b115cc48840bfe02c9ae84114afbadb2750
AllanOps <ombongiallan@gmail.com>
2023-07-11 21:42:09 +0300

    Copying the memory area - Task 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - Copies the memory area
+ * @dest: Destination memory area
+ * @src: Source memory area
+ * @n: Bytes filled
+ * Return: Pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned nt i)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + 1) + *(src + 1);
+
+	return (dest);
+}
