22cb4f687fc770e38ac96c2027a70e912932688e
AllanOps <ombongiallan@gmail.com>
2023-07-12 20:04:24 +0300

    Adding positive numbers - Task 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Adds two positive numbers
+ * @argc: Number of command line arguments
+ * @argv: Array containing program command kine args
+ * Return: 0 as success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j add = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
