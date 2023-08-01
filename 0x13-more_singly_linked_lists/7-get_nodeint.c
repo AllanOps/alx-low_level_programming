4966e7a76eea043e81bfbf64101bf4e65e8918d1
AllanOps <ombongiallan@gmail.com>
2023-08-01 23:40:34 +0300

    Printing elements of a list - Task 8

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Prints elements of a list
+ * @head: The headnode
+ * @index: Index node
+ * Return: NULL if the node is non-existant
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *z;
+	z = head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	z = head;
+
+	while (index != 0)
+	{
+		z = z->next;
+		index--;
+	}
+
+	if (z == NULL)
+	{
+		return (0);
+	}
+
+	return (z);
+}
